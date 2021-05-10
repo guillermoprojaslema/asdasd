1e5652d97df3b90badfd185a8019e72e9ab566f9
Gerardo Martinez Arpe <gema.arpe@gmail.com>
2021-05-09 21:43:47 -0400

    mejora  validacion de banco

--- evaluacion final unidad 3/js/validacion.js
+++ evaluacion final unidad 3/js/validacion.js
@@ -1,81 +1,90 @@
+
 function calculandoganancia() {
 
     var objMonto = document.getElementById("Monto");
     var objMoneda = document.getElementsByName("inlineRadioOptions");
-    var objDeposito = document.getElementById("Deposito").children;
+    var objDeposito = document.getElementById("Deposito").children
     var objPlazo = document.getElementById("Plazo");
 
+  
 
-    if (objMonto.value != "" &&
+    if ( objMonto.value != "" &&
         objMonto.value != null &&
         objPlazo.value != "" &&
         objPlazo.value != null) {
-        objGanancia = (objMonto.value * objPlazo.value * 0, 0047)
+        objGanancia = (objPlazo.value * objMonto.value * 0.0047)
         console.log("calculando ganancia");
 
-
-        for (j = 0; j < objMoneda.length; j++) {
-            var radio_moneda = objMoneda[j];
-
+        for (i = 1; i < objDeposito.length; i++) {
+            var objOption = objDeposito[i];
 
 
-            for (i = 1; i < objDeposito.length; i++) {
-                var objOption = objDeposito[i];
+            for (i = 0; i < objMoneda.length; i++) {
+                var radio_moneda = objMoneda[i];
+
 
                 if (objOption.value == "") {
                     alert(" Seleccione un tipo de inversion")
+                    break
                 }
+
+                if(radio_moneda.value = ""){
+                    alert("seleccione un tipo de moneda")
+                    break
+                }
+            
+
 
                 // Condiciones para plazo fijo
 
-                if (objOption.value == "1") {
-
-                    if (objMoneda.value == "0") {
+                if (objOption.value == "1" && radio_moneda.value == "0") {
 
 
-                        if (objPlazo.value < 7 && objPlazo.value <= 90) {
-                            alert("El tiempo de inversion es de 7 a 90 dias")
-                        }
-                        if (objMonto.value <= 10000) {
-                            alert("el monto minimo a invertir es de 10000 Pesos")
-
-                        }
+                    if (objPlazo.value < 7 && objPlazo.value > 90) {
+                        alert("El tiempo de inversion es de 7 a 90 dias");
+                        return false;
+                    }
+                    if (objMonto.value <= 10000) {
+                        alert("el monto minimo a invertir es de 10000 Pesos");
+                        return false;
 
                     }
+
+
+                    if (radio_moneda.value == "1") {
+
+                        if (objPlazo.value < 120 && objPlazo.value > 365) {
+                            alert("El tiempo de inversion es de 120 dias hasta 1 año");
+                            return false;
+                        }
+                        if (objMonto.value < 5000) {
+                            alert("El monto minimo es de 5000");
+                            return false;
+                        }
+                    }
+
 
                 }
 
                 // condiciones para plazo renovable
 
-                if (objOption.value === "2") {
-
-                    if (objMoneda.value == "1") {
+                if (objOption.value == "2" && radio_moneda.value == "1") {
 
 
-                        if (objPlazo.value >= 120 && objPlazo.value <= 365) {
-                            alert(" el tiempo de la inversion es desde 120 a 1 año")
-                        }
-                        if (objMonto.value < 5000) {
-                            alert(" monto minimo desde 5000")
-                        }
+
+                    if (objPlazo.value >= 120 && objPlazo.value <= 365) {
+                        alert("El tiempo de inversion es desde 120 dias hasta 1 año");
+                        return false;
+                    }
+                    if (objMonto.value < 5000) {
+                        alert(" El monto minimo es de 5000");
+                        return false;
                     }
 
-                }
 
+                }
             }
         }
-
-
-
-
-
-
-
-
     }
-
-
-
-
 
 }
