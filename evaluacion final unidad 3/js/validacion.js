ed74b87b78465d5839e0d91eef8528f064648096
Guillermo <guillermorojaslema@gmail.com>
2021-05-10 00:24:52 -0400

    ayuda

--- evaluacion final unidad 3/js/validacion.js
+++ evaluacion final unidad 3/js/validacion.js
@@ -1,45 +1,20 @@
 function checkDepositType() {
     var depositValue = document.getElementById("Deposito").value
-
     // Plazo Fijo
     if (depositValue == 1) {
-
         // Desbloquear UF
         document.getElementById("Uf").style.visibility = "visible";
-
     }
     // Renovable
     else {
-
         // Esconder el UF
         document.getElementById("Uf").style.visibility = "hidden";
 
         //Esto elige pesos. De esa forma, UF se oculta, y si tiene valor, �ste es reemplazado por el valor de Pesos
         document.querySelector('input[currency="Pesos"]:checked');
-
-
-        var currency = document.querySelector('input[name="currency"]:checked').value
-        depositValues = setDepositValues(currency); // CLP
-
     }
 }
 
-
-/*Vamos a asumir que es CLP*/
-
-function setDepositValues(currency) {
-
-    if (currency == 'clp') {
-        var minimunSaveDays = 7;
-        var maximumSaveDays = 90;
-        var minumumAmount = 10000;
-    } else {
-        var minimunSaveDays = 120;
-        var maximumSaveDays = 365;
-        var minumumAmount = 5000;
-    }
-    return [minimunSaveDays, maximumSaveDays, minumumAmount]
-}
 
 function calculateProfit(time, amount) {
     return time * amount * 0.0047;
@@ -54,7 +29,6 @@ function isValidSubmitForm() {
     var objDeposito = document.getElementById("Deposito").value;
     var objPlazo = document.getElementById("Plazo");
 
-
     if (objMonto.value != "" &&
         objMonto.value != null &&
         objPlazo.value != "" &&
@@ -67,7 +41,6 @@ function isValidSubmitForm() {
     }
 
     return valid;
-
 }
 
 
@@ -75,14 +48,12 @@ function calculandoganancia() {
 
     var objMonto = document.getElementById("Monto");
     var objMoneda = document.getElementsByName("currency");
-    var objDeposito = document.getElementById("Deposito").value;
     var objPlazo = document.getElementById("Plazo");
-
-
+    
     if (isValidSubmitForm()) {
         profit = calculateProfit(objPlazo.value, objMonto.value);
         alert('El valor de la ganancia es ' + profit + ' ' + objMoneda.value);
     } else {
-        // ALERT CON LOS DATOS ERR�NEOS
+        alert('Ingrese correctamente los datos');
     }
 }
